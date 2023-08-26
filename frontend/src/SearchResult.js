085672283a0cd9823d3fccbc0df6a51643488e0a
Baeg <73841260+bgyoons@users.noreply.github.com>
2023-08-26 15:59:29 +0900

    change semantic tag

--- frontend/src/SearchResult.js
+++ frontend/src/SearchResult.js
@@ -1,11 +1,14 @@
 class SearchResult {
   $searchResult = null;
+  $ul = null;
   data = null;
   onClick = null;
 
   constructor({ $target, initialData, onClick }) {
-    this.$searchResult = document.createElement("div");
+    this.$searchResult = document.createElement("section");
     this.$searchResult.className = "SearchResult";
+    this.$ul = document.createElement("ul");
+    this.$searchResult.appendChild(this.$ul);
     $target.appendChild(this.$searchResult);
 
     this.data = initialData;
@@ -20,12 +23,12 @@ class SearchResult {
   }
 
   render() {
-    this.$searchResult.innerHTML = this.data
+    this.$ul.innerHTML = this.data
       .map(
         cat => `
-          <div class="item">
+          <li class="item">
             <img src=${cat.url} alt=${cat.name} />
-          </div>
+          </li>
         `
       )
       .join("");
