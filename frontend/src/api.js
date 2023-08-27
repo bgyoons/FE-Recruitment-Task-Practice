5b84c18daa3b7cc9c965a7f56220628712493396
Baeg <73841260+bgyoons@users.noreply.github.com>
2023-08-28 00:45:53 +0900

    add api request method

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -11,5 +11,23 @@ const api = {
     } catch (error) {
       console.error(error)
     }
-  }
+  },
+  getRandomCats: async () => {
+    try {
+      const response = await fetch(`${API_ENDPOINT}/api/cats/random50`)
+      const cats = await response.json();
+      return cats;
+    } catch (error) {
+      console.error(error)
+    }
+  },
+  getCatDetail: async id => {
+    try {
+      const response = await fetch(`${API_ENDPOINT}/api/cats/${id}`)
+      const detail = await response.json();
+      return detail;
+    } catch (error) {
+      console.error(error)
+    }
+  },
 };
