779d38efbb26ddc8e7e1e8f90d2e4c04dde0522e
Baeg <73841260+bgyoons@users.noreply.github.com>
2023-08-28 00:44:08 +0900

    change api call async/await

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,9 +3,13 @@ const API_ENDPOINT =
 // "https://rhdd0roxs5.execute-api.ap-northeast-2.amazonaws.com/dev";
 
 const api = {
-  fetchCats: keyword => {
-    return fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`).then(res =>
-      res.json()
-    );
+  fetchCats: async keyword => {
+    try {
+      const response = await fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`)
+      const cats = await response.json();
+      return cats;
+    } catch (error) {
+      console.error(error)
+    }
   }
 };
