4e05f696de5c084815b29256cfd8db6482d4495c
Baeg <73841260+bgyoons@users.noreply.github.com>
2023-09-04 23:10:33 +0900

    refactor error status

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,11 +2,15 @@ const API_ENDPOINT =
   "http://localhost:4001";
 // "https://rhdd0roxs5.execute-api.ap-northeast-2.amazonaws.com/dev";
 
+const REQUEST_ERROR = {
+  '500': { message: 'request fail' }
+}
+
 const request = async url => {
   try {
     const response = await fetch(`${API_ENDPOINT}/api/cats/${url}`);
     if (response.status === 200) return response.json();
-    else throw { result: response, message: 'request fail' };
+    else throw REQUEST_ERROR[response.status];
   } catch (error) {
     console.log(error);
     alert(error.message);
